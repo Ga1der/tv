cacb50ebc7fa317e72ba6e091eef6e7acafffbae
Zakordonets Sergey <posar4@mail.ru>
2020-02-24 23:10:20 +0200

    Create Divan.init.js

---
+++ Divan.init.js
@@ -0,0 +1,11 @@
+            $(function () {
+                const divan = new Divan();
+                divan.apiAuth();
+                divan.apiChannels();
+
+                const divanUI = new DivanUI(divan);
+                divanUI.drawChannels();
+                divanUI.enableFavoriteChannals();
+                divanUI.onGetCode();
+            });
+            
