220eb8b1757a314fe5b517c0c272f4ab41a2266f
Zakordonets Sergey <posar4@mail.ru>
2020-02-24 21:54:45 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,24 @@
-self.addEventListener("fetch", function(event) {
-  const a = fetch(event.request).catch(function(error) {
-    console.log("[Service Worker] Network request Failed. Serving content from cache: " + error);
-    //Check to see if you have it in the cache
-    //Return response
-    //If not in the cache, then return error page
-    return caches
-      .open("sw-precache-v3-sw-precache-webpack-plugin-https://silent-things.surge.sh")
-      .then(function(cache) {
-        return cache.match(event.request).then(function(matching) {
-          var report =
-            !matching || matching.status == 404
-              ? Promise.reject("no-match")
-              : matching;
-          return report;
-        });
-      });
-  });
-  event.respondWith(a);
+// self.addEventListener("fetch", function(event) {
+//   const a = fetch(event.request).catch(function(error) {
+//     console.log("[Service Worker] Network request Failed. Serving content from cache: " + error);
+//     //Check to see if you have it in the cache
+//     //Return response
+//     //If not in the cache, then return error page
+//     return caches
+//       .open("sw-precache-v3-sw-precache-webpack-plugin-https://silent-things.surge.sh")
+//       .then(function(cache) {
+//         return cache.match(event.request).then(function(matching) {
+//           var report =
+//             !matching || matching.status == 404
+//               ? Promise.reject("no-match")
+//               : matching;
+//           return report;
+//         });
+//       });
+//   });
+//   event.respondWith(a);
+// });
+
+self.addEventListener('fetch', function(e){
+
 });
